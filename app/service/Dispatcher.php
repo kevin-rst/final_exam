0dc88f3c5bf75cf618ff5370fd662a51a354041c
Kev-magic <kev30razafindramananatna@gmail.com>
2026-02-17 12:02:45 +0300

    okok

--- app/service/Dispatcher.php
+++ app/service/Dispatcher.php
@@ -36,41 +36,128 @@ class Dispatcher
                     break;
                 case 'proportion':
                     $besoins = $besoinModel->getAllBesoinsByIdType($idType);
+                    break;
                 default:
                     break;
             }
 
-            foreach ($besoins as $besoin) {
-                if ($resteDon <= 0) break;
+            if ($method === 'proportion') {
+                $besoinsFiltres = [];
+                $totalBesoin = 0;
 
-                $idBesoin = $besoin['id_besoin'];
-                $idVille = $besoin['id_ville'];
-                $quantiteRestante = $besoin['quantite_restante'] ?? $besoin['quantite'];
-                $resteBesoin = $quantiteRestante;
+                foreach ($besoins as $besoin) {
+                    $quantiteRestante = $besoin['quantite_restante'] ?? $besoin['quantite'];
+                    $resteBesoin = (int) $quantiteRestante;
 
-                if ($resteBesoin <= 0) continue;
+                    if ($resteBesoin <= 0) {
+                        continue;
+                    }
 
-                if ($method === 'proportion') {
-                    $quantite = floor($resteBesoin / $resteDon);
-                } else {
+                    $besoinsFiltres[] = [
+                        'id_besoin' => $besoin['id_besoin'],
+                        'id_ville' => $besoin['id_ville'],
+                        'reste_besoin' => $resteBesoin,
+                    ];
+                    $totalBesoin += $resteBesoin;
+                }
+
+                if ($totalBesoin > 0) {
+                    $allocations = [];
+                    $quantiteDistribuee = 0;
+
+                    foreach ($besoinsFiltres as $besoin) {
+                        $raw = ($resteDon * $besoin['reste_besoin']) / $totalBesoin;
+                        $quantite = (int) floor($raw);
+                        if ($quantite > $besoin['reste_besoin']) {
+                            $quantite = $besoin['reste_besoin'];
+                        }
+                        $decimal = $raw - $quantite;
+
+                        $allocations[] = [
+                            'id_besoin' => $besoin['id_besoin'],
+                            'id_ville' => $besoin['id_ville'],
+                            'reste_besoin' => $besoin['reste_besoin'],
+                            'quantite' => $quantite,
+                            'decimal' => $decimal,
+                        ];
+                        $quantiteDistribuee += $quantite;
+                    }
+
+                    $resteARepartir = $resteDon - $quantiteDistribuee;
+                    if ($resteARepartir > 0) {
+                        usort($allocations, function ($a, $b) {
+                            if ($a['decimal'] === $b['decimal']) {
+                                return $a['id_besoin'] <=> $b['id_besoin'];
+                            }
+                            return $b['decimal'] <=> $a['decimal'];
+                        });
+
+                        $didAllocate = true;
+                        while ($resteARepartir > 0 && $didAllocate) {
+                            $didAllocate = false;
+                            foreach ($allocations as $index => $allocation) {
+                                if ($resteARepartir <= 0) {
+                                    break;
+                                }
+                                if ($allocation['quantite'] < $allocation['reste_besoin']) {
+                                    $allocations[$index]['quantite'] += 1;
+                                    $resteARepartir -= 1;
+                                    $didAllocate = true;
+                                }
+                            }
+                        }
+                    }
+
+                    $totalDistribue = 0;
+                    foreach ($allocations as $allocation) {
+                        if ($allocation['quantite'] <= 0) {
+                            continue;
+                        }
+
+                        $distributionModel->createDistribution([
+                            'don' => $idDon,
+                            'ville' => $allocation['id_ville'],
+                            'type' => $idType,
+                            'quantite' => $allocation['quantite'],
+                            'achat_source' => null,
+                            'type_distribution' => 'don_direct'
+                        ]);
+
+                        $nouvelleQuantiteRestante = $allocation['reste_besoin'] - $allocation['quantite'];
+                        $besoinModel->updateQuantiteRestante($allocation['id_besoin'], $nouvelleQuantiteRestante);
+
+                        $totalDistribue += $allocation['quantite'];
+                    }
+
+                    $resteDon -= $totalDistribue;
+                }
+            } else {
+                foreach ($besoins as $besoin) {
+                    if ($resteDon <= 0) break;
+
+                    $idBesoin = $besoin['id_besoin'];
+                    $idVille = $besoin['id_ville'];
+                    $quantiteRestante = $besoin['quantite_restante'] ?? $besoin['quantite'];
+                    $resteBesoin = $quantiteRestante;
+
+                    if ($resteBesoin <= 0) continue;
+
                     $quantite = min($resteDon, $resteBesoin);
-                }
 
-                $distributionModel->createDistribution([
-                    'don' => $idDon,
-                    'ville' => $idVille,
-                    'type' => $idType,
-                    'quantite' => $quantite,
-                    'achat_source' => null,
-                    'type_distribution' => 'don_direct'
-                ]);
+                    $distributionModel->createDistribution([
+                        'don' => $idDon,
+                        'ville' => $idVille,
+                        'type' => $idType,
+                        'quantite' => $quantite,
+                        'achat_source' => null,
+                        'type_distribution' => 'don_direct'
+                    ]);
 
-                if ($method !== 'proportion') {
                     $resteDon -= $quantite;
-                }
 
-                $nouvelleQuantiteRestante = $resteBesoin - $quantite;
-                $besoinModel->updateQuantiteRestante($idBesoin, $nouvelleQuantiteRestante);
+                    $nouvelleQuantiteRestante = $resteBesoin - $quantite;
+                    $besoinModel->updateQuantiteRestante($idBesoin, $nouvelleQuantiteRestante);
+                }
             }
 
             if ($resteDon <= 0) {
